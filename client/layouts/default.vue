9cdebed36a01d48741a4ede7033b2a9e62467182
pignuante <syaoran215@naver.com>
2020-06-24 15:33:51 +0900

    standard vuex

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,31 +1,32 @@
 <template>
   <v-app>
-    <nav>
-      <v-app-bar app dark color="green">
-        <v-toolbar-title>
-          <nuxt-link to="/">
-            Home
-          </nuxt-link>
-        </v-toolbar-title>
-        <v-spacer />
-        <v-toolbar-items>
-          <v-text-field
-            label="검색"
-            hide-details
-            prepend-icon="mdi-magnify"
-            :style="{ display: 'flex', alignItems: 'center' }"
-          />
-          <v-btn text nuxt to="/profile">
-            프로필
-          </v-btn>
-          <v-btn text nuxt to="/signup">
-            회원가입
-          </v-btn>
-        </v-toolbar-items>
-      </v-app-bar>
-    </nav>
+    <v-app-bar app dark color="green">
+      <v-toolbar-title>
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items>
+        <v-text-field
+          label="검색"
+          hide-details
+          prepend-icon="mdi-magnify"
+          :style="{ display: 'flex', alignItems: 'center' }"
+        />
+        <v-btn text nuxt to="/profile">
+          프로필
+        </v-btn>
+        <v-btn text nuxt to="/signup">
+          회원가입
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
 
     <v-main>
+      <v-btn @click="onChangeName">
+        <div>{{ name }}</div>
+      </v-btn>
       <v-container>
         <v-row>
           <v-col cols="12" md="4">
@@ -46,6 +47,16 @@ import LoginForm from "~/components/LoginForm"
 export default {
   components: {
     LoginForm,
+  },
+  computed: {
+    name() {
+      return this.$store.state.posts.name
+    },
+  },
+  methods: {
+    onChangeName() {
+      this.$store.commit("posts/BYE")
+    },
   },
   head() {
     return {
