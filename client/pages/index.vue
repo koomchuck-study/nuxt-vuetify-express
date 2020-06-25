3ece0a2ac97f13df4935f8e29556ae3cfeefe71c
pignuante <syaoran215@naver.com>
2020-06-25 21:28:11 +0900

    infinity scroll

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -16,6 +16,9 @@ export default {
     PostForm,
     PostCard,
   },
+  asyncData({ store }) {
+    store.dispatch("posts/loadPosts")
+  },
   data() {
     return {
       name: "julia.js",
@@ -27,6 +30,27 @@ export default {
     },
     mainPosts() {
       return this.$store.state.posts.mainPosts
+    },
+    hasMorePost() {
+      return this.$store.state.posts.hasMorePost
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll)
+  },
+  methods: {
+    onScroll() {
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+      ) {
+        if (this.hasMorePost) {
+          this.$store.dispatch("posts/loadPosts")
+        }
+      }
     },
   },
 }
--- client/store/posts.js
+++ client/store/posts.js
@@ -1,5 +1,6 @@
 export const state = () => ({
   mainPosts: [],
+  hasMorePost: true,
 })
 
 export const mutations = {
@@ -14,8 +15,26 @@ export const mutations = {
     const index = state.mainPosts.findIndex((v) => v.id === payload.postId)
     state.mainPosts[index].Comments.unshift(payload)
   },
+  loadPosts(state) {
+    const diff = totalPosts - state.mainPosts.length
+    const fakePosts = Array(diff > limit ? limit : diff)
+      .fill()
+      .map(() => ({
+        id: Math.random().toString(),
+        User: {
+          id: 1,
+          nickname: "pignu",
+        },
+        content: `scroll ${Math.random()}`,
+        Comments: [],
+        Images: [],
+      }))
+    state.mainPosts = state.mainPosts.concat(fakePosts)
+    state.hasMorePost = fakePosts.length === limit
+  },
 }
-
+const totalPosts = 101
+const limit = 10
 export const actions = {
   add({ commit }, payload) {
     commit("addMainPost", payload)
@@ -25,5 +44,10 @@ export const actions = {
   },
   addComment({ commit }, payload) {
     commit("addComment", payload)
+  },
+  loadPosts({ commit, state }) {
+    if (state.hasMorePost) {
+      commit("loadPosts")
+    }
   },
 }
